88bb4e6dce396f29a81110e6a5fa933ba978bd7c
pratapy <pratapyalamanchi.1@gmail.com>
2022-02-10 21:44:53 -0500

    Initial commit

---
+++ src/main/java/io/shiprock/orderservice/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package io.shiprock.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//@SpringBootApplication//(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
